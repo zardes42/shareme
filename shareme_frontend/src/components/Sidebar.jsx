4dfc3abf8e4adf363e301955dd95cd2b28a0b093
Iyinoluwa <iyinoyelz69@gmail.com>
2022-11-28 23:59:03 -0700

    adds sidebar for desktop

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {NavLink , Link} from 'react-router-dom'
+import {RiHomeFill } from 'react-icons/ri'
+import {IoIosArrowForward} from 'react-icons/io'
+import logo from '../assets/logo.png'
 
-const Sidebar = () => {
+const Sidebar = ({user,closeToggle}) => {
+  const handleCloseSidebar =() => {
+    if(closeToggle) closeToggle(false)
+  }
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar '>
+      <div className="flex flex-col ">
+        <Link to={'/'} onClick={ handleCloseSidebar} className="flex px-5 gap-2 my-6 pt-1 w-190 items-center">
+          <img src={logo} alt="logo" className='w-full' />
+        </Link>
+      </div>
+    </div>
   )
 }
 
