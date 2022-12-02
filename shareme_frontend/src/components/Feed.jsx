fa5c822d40a70a4718c9642286bc6289f965e476
Iyinoluwa <iyinoyelz69@gmail.com>
2022-12-01 23:38:49 -0700

    adds masonry layout

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,16 +1,36 @@
 import React,{useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import {client} from '../client'
+import { searchQuery,feedQuery } from '../utils/data'
 import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
 
+
 const Feed = () => {
 
-  const [loading,setLoading] = useState(true);
+  const [loading,setLoading] = useState(false);
+  const [pins, setPins] = useState(null)
+  const {categoryId} = useParams();
+  useEffect(() =>{
+    setLoading(true);
+    if(categoryId){
+      const query = searchQuery(categoryId)
+      client.fetch(query).then(data => {
+        setPins(data)
+        setLoading(false)
+      })
+    }else{
+      client.fetch(feedQuery)
+      .then(data => {
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  },[categoryId])
 
   if(loading) return <Spinner message="We are adding new ideas to your field" />
   return (
-    <div>Feed</div>
+    <div>{pins && <MasonryLayout pins={pins} />}</div>
   )
 }
 
--- shareme_frontend/src/components/MasonryLayout.jsx
+++ shareme_frontend/src/components/MasonryLayout.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Masonry from 'react-masonry-css'
+import Pin from './Pin'
+const breakPointObj ={
+  default :4,
+  3000:6,
+  2000 :5,
+  1200:3,
+  1000:2,
+  500:1
+}
 
-const MasonryLayout = () => {
+const MasonryLayout = ({pins}) => {
   return (
-    <div>MasonryLayout</div>
+    <div>
+      <Masonry className="flex animate-slide-fwd " breakPointCols={breakPointObj} >
+        {pins?.map(pin =>(
+          <Pin key={pin._id} pin={pin} className="w-max" />
+        ))}
+      </Masonry>
+    </div>
   )
 }
 
