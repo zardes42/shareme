5cfa8e5adfdc5473f9e2fde23565585abf944dc6
Iyinoluwa <iyinoyelz69@gmail.com>
2022-12-08 11:57:45 -0700

    fixes comment type error

--- shareme_backend/schemas/comment.js
+++ shareme_backend/schemas/comment.js
@@ -6,7 +6,7 @@ export default {
         {
             name: 'comment',
             title:'Comment',
-            type:'postedBy'
+            type:'string'
         },
         {
             name: 'postedBy',
