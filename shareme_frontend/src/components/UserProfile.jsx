664bad2e9efb10d873d4d22ae87040819d8b63f0
Iyinoluwa <iyinoyelz69@gmail.com>
2022-12-09 14:58:34 -0700

    completes user proflie layout

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -14,6 +14,10 @@ import Spinner from "./Spinner";
 
 const url =
   "https://source.unsplash.com/1600x900/?nature,photography,technology";
+const activeBtnStyles =
+  "bg-red-500 text-white font-bold p-2 rounded-full  w-20 outline-none";
+const notActiveBtnStyles =
+  "bg-primary mr-4 text-black font-bold p-2 rounded-full  w-20 outline-none";
 const UserProfile = () => {
   const [user, setUser] = useState(null);
   const [pins, setPins] = useState(null);
@@ -29,10 +33,26 @@ const UserProfile = () => {
       setUser(data[0]);
     });
   }, [userId]);
-  const logout =()=>{
+  const logout = () => {
     localStorage.clear();
-    navigate('./login')
-  }
+    navigate("./login");
+  };
+  useEffect(() =>{
+    if (text === 'created'){
+      const createdPinsQuery = userCreatedPinsQuery(userId)
+      client
+      .fetch(createdPinsQuery)
+      .then((data) => {
+        setPins(data)
+      })}else{
+        const savedPinsQuery = userSavedPinsQuery(userId)
+        client
+        .fetch(savedPinsQuery)
+        .then((data) => {
+          setPins(data)
+        })
+      }
+  },[text,userId]);
 
   if (!user) return <Spinner message="Loading Profile" />;
 
@@ -56,29 +76,61 @@ const UserProfile = () => {
             </h1>
             <div className="absolute top-0 z-1 p-2 right-0">
               {userId === user._id && (
-              <GoogleLogout
-                clientId={import.meta.env.VITE_GOOGLE_API_ID}
-                render={(renderProps) => (
-                  <button
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                    type="button"
-                    className="bg-white outline-none items-center p-2 rounded-full cursor-pointer shadow-md "
-                  >
-                    <AiOutlineLogout color='red' fontSize={21} />
-                  </button>
-                )}
-                
-                onLogoutSuccess={logout}
-                cookiePolicy='single_host_origin'
-                
-              />
+                <GoogleLogout
+                  clientId={import.meta.env.VITE_GOOGLE_API_ID}
+                  render={(renderProps) => (
+                    <button
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                      type="button"
+                      className="bg-white outline-none items-center p-2 rounded-full cursor-pointer shadow-md "
+                    >
+                      <AiOutlineLogout color="red" fontSize={21} />
+                    </button>
+                  )}
+                  onLogoutSuccess={logout}
+                  cookiePolicy="single_host_origin"
+                />
               )}
             </div>
           </div>
+          <div className="text-center mb-7 ">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("created");
+              }}
+              className={
+                activeBtn === "created" ? activeBtnStyles : notActiveBtnStyles
+              }
+            >
+              Created
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("saved");
+              }}
+              className={
+                activeBtn === "saved" ? activeBtnStyles : notActiveBtnStyles
+              }
+            >
+              Saved
+            </button>
+          </div>
+         { pins?.length ? (<div className='px-2 '>
+            <MasonryLayout pins={pins} />
+
+          </div>) :(
+            <div className='flex justify-center font-bold w-full text-xl items-center m-2'>
+
+            </div>
+          )}
         </div>
       </div>
-      UserProfile
+
     </div>
   );
 };
