96df809e0c1c0fdef7d10f9be6a32a89830f9309
Iyinoluwa <iyinoyelz69@gmail.com>
2022-12-09 14:57:34 -0700

    adds image to categories

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -4,20 +4,7 @@ import {RiHomeFill } from 'react-icons/ri'
 import {IoIosArrowForward} from 'react-icons/io'
 import logo from '../assets/logo.png'
 
-const categories = [
-  {
-    name:'Animals'
-  },
-  {
-    name:'Photography'
-  },{
-    name:'Wallpapers'
-  },{
-    name:'Gaming'},
-    {
-    name:'Coding'
-  },
-]
+import {categories} from '../utils/data'
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize-text '
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize-text '
@@ -39,6 +26,7 @@ const Sidebar = ({user,closeToggle}) => {
           <h3 className="mt-2 px-5 text-base 2xl:text-xl"> Discover Categories</h3>
           {categories.slice(0, categories.length - 1).map(category => (
             <NavLink onClick={ handleCloseSidebar} key={category.name} to={`/category/${category.name}`}  className={({isActive})=> isActive ? isActiveStyle : isNotActiveStyle}>
+              <img src={category.image} className='w-8 h-8 rounded-full shadow-sm ' alt="category" />
               {category.name}
             </NavLink>
           ))}
